f8c9db1d2ccb4bed89526d1371a3f27bdf06092a
Fich019 <44195783+Fich019@users.noreply.github.com>
2018-10-30 17:45:26 +0000

    Started Companion code
    Detection

---
+++ Assets/Scripts/Detection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour {
+
+    public SmoothLookAtTarget2D lookAt;
+
+    public List<Transform> enemies = new List<Transform>();
+
+	void Start ()
+    {
+        InvokeRepeating("Shoot", 0, 2);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!enemies.Contains(collision.transform))
+        {
+            enemies.Add(collision.transform);
+        }
+    }
+
+    void Shoot()
+    {
+        Transform closest = null;
+        float dist = 9999;
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if(Vector3.Distance(transform.position, enemies[i].position) < dist)
+            {
+                closest = enemies[i];
+                dist = Vector3.Distance(transform.position, enemies[i].position);
+            }
+        }
+
+
+        if(closest != null)
+        {
+            lookAt.target = closest;
+        }
+
+    }
+}
