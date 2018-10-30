6ba52e853daf2a0161ff115f1bd5549b4abfb1af
Fich019 <44195783+Fich019@users.noreply.github.com>
2018-10-30 17:16:32 +0000

    Added second weeks tutorial
    Zombie,
    Health,
    Zombie move,
    Shoot

---
+++ Assets/Scripts/MoveTowardsObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsObject : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);
+        }
+	}
+}
