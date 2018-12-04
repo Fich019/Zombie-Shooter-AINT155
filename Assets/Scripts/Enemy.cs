4e3c9dd0f0104a6c46ee7a2c97fed3b11be45148
Fich019 <44195783+Fich019@users.noreply.github.com>
2018-12-04 17:31:44 +0000

    added melee with knockback

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,14 +11,29 @@ public class Enemy : MonoBehaviour {
 
     public EnemySpawnedEvent onSpawn;
 
+
 	// Use this for initialization
 	void Start () {
         GameObject player = GameObject.FindWithTag("Player");
         onSpawn.Invoke(player.transform);
+        
 	}
 	
-	// Update is called once per frame
-	void Update () {
+    public void DoKnockBack()
+    {
+        GetComponent<Rigidbody2D>().AddForce(-transform.up * 7, ForceMode2D.Impulse);
+        GetComponent<MoveTowardsObject>().enabled = false;
+        Invoke("Stopknockback", 1);
+    }
+    private void Stopknockback()
+    {
+       GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<MoveTowardsObject>().enabled = true;
+        print("stop knock back");
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
---
+++ Assets/Scripts/Melee.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour {
+
+    private bool isswing = false;
+    public int damage = 2;
+    public float swingdelay = 4f;
+    public float speed = 5.0f;
+
+    Transform enemy;
+
+
+
+    private void Setswing()
+    {
+        isswing = false;
+        GetComponent<Collider2D>().enabled = false;
+    } 
+
+    private void Attack()
+    {
+        isswing = true;
+        GetComponent<Collider2D>().enabled = true;
+        Invoke("Setswing", swingdelay);
+    }
+    // Use this for initialization
+    void Start () {
+        GetComponent<Collider2D>().enabled = false;
+    }
+
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        enemy = collision.transform;
+        // apply damage to enemy hit
+        //print(collision.transform);
+        enemy.SendMessage("TakeDamage", damage);
+        //Vector3 facing = -enemy.up;
+
+        
+        //if (enemy.GetComponent<Rigidbody2D>().velocity == Vector2.zero)
+        //{
+            //enemy.GetComponent<Rigidbody2D>().AddForce(facing * 500);
+            enemy.SendMessage("DoKnockBack");
+        
+        //}
+        
+
+        //Invoke("Stopknockback", 0.5f);
+        
+        
+        
+        //Vector3.MoveTowards(Currentpos.position, collision.transform.position, speed);
+    }
+
+    //private void Stopknockback()
+    //{
+    //    if (enemy != null)
+    //    {
+    //        enemy.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    //    }
+    //    //print("stop knock back");
+    //}
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isswing)
+            {
+                Attack();
+            }
+        }
+	}
+}
