853475a7f006a6f25f8968989e742655a863fae1
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-01-15 16:43:57 +0000

    menu + music

---
+++ Assets/Scenefade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenefade : MonoBehaviour {
+
+    AudioSource Click;
+    public Animator sceneanim;
+
+    private void Start()
+    {
+        Click = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetMouseButtonDown(0)){
+            Click.Play();
+            FadetoLevel(1);
+        }
+	}
+
+    public void FadetoLevel (int LevelIndex)
+    {
+        sceneanim.SetTrigger("FadeIn");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
---
+++ Assets/StopMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopMusic : MonoBehaviour {
+
+    AudioSource StopMus;
+	// Use this for initialization
+	void Start () {
+        StopMus = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StopMus.Stop();
+        }
+    }
+}
