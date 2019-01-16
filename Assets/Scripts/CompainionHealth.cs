0d0a6cbdfb4903ecb928f2c7c8db4ef6884e1fa9
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-01-16 16:07:18 +0000

    pathfinding

---
+++ Assets/Scripts/CompainionHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompainionHealth : MonoBehaviour {
+
+    public delegate void UpdateHealth(int newHealth);
+    public static event UpdateHealth OnUpdateHealth;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SendHealthData(int health)
+    {
+        if (OnUpdateHealth != null)
+        {
+            OnUpdateHealth(health);
+        }
+    }
+}
